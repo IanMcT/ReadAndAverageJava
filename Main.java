1d51bec31a1749b465767c1fe0b98359aa79ae7e
IanMcT <ian.mctavish@tldsb.on.ca>
2022-10-25 18:04:36 +0000

    Averaged the numbers

--- Main.java
+++ Main.java
@@ -3,15 +3,22 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
+    int total = 0;
+    int counter = 0;
     try{
+    
       File theFile = new File("numbers.txt");
       Scanner fileScanner = new Scanner(theFile);
       while(fileScanner.hasNextLine()){
         int x = fileScanner.nextInt();
+        total += x;
+        counter++;
         fileScanner.nextLine();
         System.out.println(x);
       }
       System.out.println("running");
     }catch(Exception e){}
+    int average = total/counter;
+    System.out.println(average + " average");
   }
 }
